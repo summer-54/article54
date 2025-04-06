c9115ca6ffb3c252d7febf0d4db3329bfc7dfb40
sizoff <sizoff.dev@yandex.ru>
2025-04-06 13:44:47 +0300

    Gallery.vue

---
+++ src/article54/Gallery0.vue
@@ -0,0 +1,72 @@
+<script lang="ts">
+import {Galleria} from "primevue";
+import type {PropType} from "vue";
+
+export default {
+  name: "Gallery",
+  components: {Galleria},
+  props: {
+    images: {
+      type: Array as PropType<Array<{src: string, thumb: string, alt: string}>>,
+      required: true
+    }
+  },
+  data() {
+    return {
+      activeIndex: 0,
+      responsiveOptions: [
+        {
+          breakpoint: '1024px',
+          numVisible: 5
+        },
+        {
+          breakpoint: '768px',
+          numVisible: 3
+        },
+        {
+          breakpoint: '560px',
+          numVisible: 1
+        }
+      ],
+      displayCustom: false
+    };
+  },
+  methods: {
+    imageClick(index) {
+      this.activeIndex = index;
+      this.displayCustom = true;
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <Galleria v-model:activeIndex="activeIndex" v-model:visible="displayCustom" :value="images" :responsiveOptions="responsiveOptions" :numVisible="7"
+              containerStyle="max-width: 850px" :circular="true" :fullScreen="true" :showItemNavigators="true" :showThumbnails="false">
+      <template #item="slotProps">
+        <img :src="slotProps.item.src" :alt="slotProps.item.alt" style="width: 100%; display: block"/>
+      </template>
+      <template #thumbnail="slotProps">
+        <img :src="slotProps.item.thumb" :alt="slotProps.item.alt" style="display: block"/>
+      </template>
+    </Galleria>
+    <div v-if="images" class="thumbs">
+      <img v-for="(image, index) of images" :key="index" class="thumb" :src="image.thumb" :alt="image.alt" style="cursor: pointer" @click="imageClick(index)"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.thumbs {
+  display: inline-flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+}
+
+.thumb {
+  height: 160px;
+  border-radius: 10px;
+}
+</style>
--- src/article54/index.ts
+++ src/article54/index.ts
@@ -3,11 +3,13 @@ import Tabs_ from "@/article54/Tabs.vue";
 import Colored_ from "@/article54/Colored.vue";
 import Spoiler_ from "@/article54/Spoiler.vue";
 import Image_ from "@/article54/Image.vue";
+import Gallery_ from "@/article54/Gallery.vue";
 
 export const Article54 = Article54_;
 export const Tabs = Tabs_;
 export const Colored = Colored_;
 export const Spoiler = Spoiler_;
 export const Image = Image_;
+export const Gallery = Gallery_;
 
 export {TabPanel} from "primevue";
