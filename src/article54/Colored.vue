64ec5bcd06621a3572aa67bcc643bf8bea2edc88
sizoff <sizoff.dev@yandex.ru>
2025-04-06 12:38:55 +0300

    Colored.vue

---
+++ src/article54/Colored.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import {Panel} from "primevue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "ColoredComponent",
+  components: {Panel},
+  props: {
+    color: {
+      type: String,
+      default: "green"
+    }
+  }
+})
+</script>
+
+<template>
+<!--  <glm-content-area class="content-area" :bg="(attributes ?? {}).color" :clear="nested">-->
+<!--&lt;!&ndash;    <article-renderer0 :data="elements"/>&ndash;&gt;-->
+<!--    <slot/>-->
+<!--  </glm-content-area>-->
+  <Panel :style="{'--p-panel-background': color}" class="colored">
+    <slot/>
+  </Panel>
+</template>
+
+<style>
+.colored .p-panel-header {
+  display: none;
+}
+
+.colored .p-panel-content {
+  padding: 10px;
+}
+</style>
--- src/article54/index.ts
+++ src/article54/index.ts
@@ -1,7 +1,9 @@
 import Article54_ from "@/article54/Article54.vue";
 import Tabs_ from "@/article54/Tabs.vue";
+import Colored_ from "@/article54/Colored.vue";
 
 export const Article54 = Article54_;
 export const Tabs = Tabs_;
+export const Colored = Colored_;
 
 export {TabPanel} from "primevue";
